bf28acf772694f51c4299e0e36a9c03f0ca42829
Frank Luan <franklsf95@gmail.com>
2014-04-10 19:18:36 -0500

    auto git commit and push when deploying

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,11 @@ echo 'Building site...'
 jekyll build
 echo 'Syncing with server...'
 rsync -v -a _site/ me:~/www/franklsf.org
+
+if [ -n "$1" ]
+    then
+        echo 'Git commiting...'
+        git commit -m "$1"
+        echo 'Git pushing...'
+        git push origin master
+fi
