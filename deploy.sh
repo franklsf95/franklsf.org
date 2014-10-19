70daa4f40e8452941f722d328e011c9e60beb5d9
Frank LSF <franklsf95@gmail.com>
2014-10-19 14:21:10 -0500

    Font asset is now https-complaint

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ rsync -v -a _site/ me:~/www/franklsf.org
 if [ -n "$1" ]
     then
         echo 'Git commiting...'
+        git add . --force
         git commit -m "$1"
-        echo 'Git pushing...'
         git push origin master
 fi
